18885896934129b4cb65297646ac123c3750a16d
BjBodner <benjybo7@gmail.com>
2021-08-21 15:13:18 +0300

    added box boundry with tests

--- src/distributed_reflectors/boundries.py
+++ src/distributed_reflectors/boundries.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+# TODO move this to utils
+def parse_coordinates(particle_coordinates: np.ndarray) -> np.ndarray:
+    particles_x = particle_coordinates[:, 0]
+    particles_y = particle_coordinates[:, 1]
+    particles_angles = particle_coordinates[:, 2]
+
+    return particles_x, particles_y, particles_angles
+
+
 class BoxBoundry:
     """a box centered at zero, with a fixed height and width.
     ranges of allowed values within box are between [-width / 2, width / 2] in the x direction
@@ -8,43 +17,124 @@ class BoxBoundry:
     """
 
     def __init__(self, height: float, width: float) -> None:
+
+        self._check_inputs(height, width)
         self.height = height
         self.width = width
+        self.max_radius = np.sqrt(height ** 2 + width ** 2)
 
-    def _get_non_collided_particle_indices(all_colisions: np.ndarray) -> np.ndarray:
+    def _check_inputs(self, height: float, width: float) -> None:
+        # check inputs
+        if height < 0:
+            raise ValueError(
+                f"height argument must be positive float, but got value height = {height}"
+            )
+        if width < 0:
+            raise ValueError(
+                f"width argument must be positive float, but got value width = {width}"
+            )
+
+    def _get_non_collided_particle_indices(
+        self, all_colisions: np.ndarray
+    ) -> np.ndarray:
         non_collided_particle_indices = np.nonzero(all_colisions == 0)[0]
         return non_collided_particle_indices
 
-    def _calc_distance_to_x_boundry(self, theta_p: np.ndarray) -> np.ndarray:
-        pass
+    def _calc_x_collision_coordinates(
+        self, non_collided_particles_coordinates: np.ndarray
+    ) -> np.ndarray:
 
+        particles_x, particles_y, particles_angles = parse_coordinates(
+            non_collided_particles_coordinates
+        )
 
-def get_final_coordinates(H, W, particle_coordinates, all_colisions):
+        # calc coordinates in case of x collision
+        max_x_displacement = self.max_radius * np.cos(particles_angles)
+        x_final = particles_x + max_x_displacement
+        allowed_x_final = np.clip(x_final, -self.width / 2, self.width / 2)
+        allowed_y_displacement = allowed_x_final * np.tan(particles_angles)
+        y_final = particles_y + allowed_y_displacement
 
-    # get indices of active particles
-    noncollided_particles_idx = np.nonzero(all_colisions == 0)[0]
+        # check if a valid x boundry collision occured
+        x_boundry_reached = np.abs(allowed_x_final) == (self.width / 2)
+        y_value_within_allowed_range = np.abs(y_final) <= (self.height / 2)
+        valid_x_boundry_collision = np.logical_and(
+            x_boundry_reached, y_value_within_allowed_range
+        )
+        valid_x_boundry_collision = np.expand_dims(
+            valid_x_boundry_collision.astype(int), 1
+        )
 
-    theta_p = particle_coordinates[noncollided_particles_idx, 2]
-    R = 2 * np.sqrt(H ** 2 + W ** 2)
+        # for cases of a valid x collision, calculate the resulting coordinates
+        # all other coordinates that did not have this collision will be set to zero
+        x_collision_coordinates = np.vstack((allowed_x_final, y_final)).T
+        x_collision_coordinates = (
+            x_collision_coordinates * valid_x_boundry_collision
+        )  # zero out all coordinates that did not have an x collision
 
-    # x boundry reached
-    x1 = np.clip(R * np.cos(theta_p), -W / 2, W / 2)
-    y1 = x1 * np.tan(theta_p)
-    x_col_coordinates = np.vstack((x1, y1)).T
-    R1_square = x1 ** 2 + y1 ** 2
+        return x_collision_coordinates, valid_x_boundry_collision
 
-    # y boundry reached
-    y2 = np.clip(R * np.sin(theta_p), -H / 2, H / 2)
-    x2 = y2 / (np.tan(theta_p) + 10 ** -5)
-    y_col_coordinates = np.vstack((x2, y2)).T
-    R2_square = x2 ** 2 + y2 ** 2
+    def _calc_y_collision_coordinates(
+        self, non_collided_particles_coordinates: np.ndarray
+    ) -> np.ndarray:
 
-    # check which collision occured
-    x_collision = np.expand_dims((R1_square < R2_square).astype(int), 1)
-    y_collision = np.expand_dims((R1_square > R2_square).astype(int), 1)
+        particles_x, particles_y, particles_angles = parse_coordinates(
+            non_collided_particles_coordinates
+        )
 
-    particle_coordinates[noncollided_particles_idx, :2] = (
-        x_col_coordinates * x_collision + y_col_coordinates * y_collision
-    )
+        # calc coordinates in case of y collision
+        max_y_displacement = self.max_radius * np.sin(particles_angles)
+        y_final = particles_y + max_y_displacement
+        allowed_y_final = np.clip(y_final, -self.height / 2, self.height / 2)
+        allowed_x_displacement = allowed_y_final / np.tan(particles_angles)
+        x_final = particles_x + allowed_x_displacement
 
-    return particle_coordinates
+        # check if a valid y boundry collision occured
+        y_boundry_reached = np.abs(allowed_y_final) == (self.height / 2)
+        x_value_within_allowed_range = np.abs(x_final) <= (self.width / 2)
+        valid_y_boundry_collision = np.logical_and(
+            y_boundry_reached, x_value_within_allowed_range
+        ).astype(int)
+        valid_y_boundry_collision = np.expand_dims(
+            valid_y_boundry_collision.astype(int), 1
+        )
+
+        # for cases of a valid y collision, calculate the resulting coordinates
+        # all other coordinates that did not have this collision will be set to zero
+        y_collision_coordinates = np.vstack((x_final, allowed_y_final)).T
+        y_collision_coordinates = y_collision_coordinates * valid_y_boundry_collision
+
+        return y_collision_coordinates, valid_y_boundry_collision
+
+    def get_final_coordinates(
+        self,
+        particles_coordinates: np.ndarray,
+        non_collided_particle_indices: np.ndarray,
+    ):
+
+        new_particle_coordinates = np.copy(particles_coordinates)
+        non_collided_particles_coordinates = particles_coordinates[
+            non_collided_particle_indices, :
+        ]
+
+        # calculates coordinates in cases of x and y collisions
+        # all particles that did not collide with reflectors are expected to reach the boundry
+        (
+            x_collision_coordinates,
+            valid_x_boundry_collision,
+        ) = self._calc_x_collision_coordinates(non_collided_particles_coordinates)
+        (
+            y_collision_coordinates,
+            valid_y_boundry_collision,
+        ) = self._calc_y_collision_coordinates(non_collided_particles_coordinates)
+
+        # replace the x,y coordinates of the
+        new_particle_coordinates[non_collided_particle_indices, :2] = (
+            x_collision_coordinates + y_collision_coordinates
+        )
+
+        return (
+            new_particle_coordinates,
+            valid_x_boundry_collision,
+            valid_y_boundry_collision,
+        )
---
+++ tests/unit/test_box_boundry.py
@@ -0,0 +1,175 @@
+import os
+import random
+import sys
+
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+sys.path.append("\\".join(os.path.dirname(__file__).split("\\")[:-2]))
+sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
+
+
+from src.distributed_reflectors.boundries import BoxBoundry
+
+SEED = 0
+
+
+def create_system(
+    box_width,
+    box_height,
+    particle_width,
+    particle_height,
+    initial_angle,
+    angle_amplitude,
+    num_particles,
+):
+    random.seed(SEED)
+    np.random.seed(SEED)
+
+    # generate box
+    box_boundry = BoxBoundry(box_width, box_height)
+
+    # generate particles
+    particle_coordinates = np.zeros((num_particles, 3))
+    particle_coordinates[:, 0] = -particle_width / 2
+    particle_coordinates[:, 1] = particle_height * (np.random.rand(num_particles) - 0.5)
+    particle_coordinates[:, 2] = initial_angle + angle_amplitude * (
+        np.random.rand(num_particles) - 0.5
+    )
+
+    # simulate collisions with reflectors
+    non_collided_particle_indices = random.sample(
+        range(num_particles), int(num_particles / 2)
+    )
+    non_collided_particle_indices = np.array(non_collided_particle_indices)
+    reflector_collided_particles_indices = [
+        i for i in range(num_particles) if i not in non_collided_particle_indices
+    ]
+
+    return (
+        box_boundry,
+        particle_coordinates,
+        non_collided_particle_indices,
+        reflector_collided_particles_indices,
+    )
+
+
+def test_instantiation():
+    box_boundry = BoxBoundry(3.0, 3.0)
+    assert isinstance(box_boundry, BoxBoundry)
+
+
+def test_x_boundry_reached():
+
+    box_width = 4.0
+    box_height = 4.0
+    num_particles = 10
+    particle_width = 3
+    particle_height = 3
+    initial_angle = 0.0
+    angle_amplitude = 0.1
+
+    (
+        box_boundry,
+        particle_coordinates,
+        non_collided_particle_indices,
+        reflector_collided_particles_indices,
+    ) = create_system(
+        box_width,
+        box_height,
+        particle_width,
+        particle_height,
+        initial_angle,
+        angle_amplitude,
+        num_particles,
+    )
+
+    # find collisions with boundry
+    (
+        new_particle_coordinates,
+        valid_x_boundry_collision,
+        valid_y_boundry_collision,
+    ) = box_boundry.get_final_coordinates(
+        particle_coordinates, non_collided_particle_indices
+    )
+
+    # either x or y collision occured for all relevant indices
+    assert np.all(np.logical_or(valid_x_boundry_collision, valid_y_boundry_collision))
+
+    # no x AND y collisions occured
+    assert np.all(valid_x_boundry_collision != valid_y_boundry_collision)
+
+    # assert only x collisions occured
+    assert np.all(valid_x_boundry_collision)
+
+    # check coordinates of boundry collision particles
+    x_coordinates = new_particle_coordinates[non_collided_particle_indices, 0]
+    y_coordinates = new_particle_coordinates[non_collided_particle_indices, 1]
+    assert np.all(np.abs(x_coordinates) == (box_width / 2))
+    assert np.all(np.abs(y_coordinates) <= (box_height / 2))
+
+    # check coordinates of reflector collided particles are left unchanged
+    assert np.all(
+        new_particle_coordinates[reflector_collided_particles_indices, :]
+        == particle_coordinates[reflector_collided_particles_indices, :]
+    )
+
+
+def test_y_boundry_reached():
+    box_width = 4.0
+    box_height = 4.0
+    num_particles = 10
+    particle_width = 3
+    particle_height = 3
+    initial_angle = np.pi / 2
+    angle_amplitude = 0.1
+
+    (
+        box_boundry,
+        particle_coordinates,
+        non_collided_particle_indices,
+        reflector_collided_particles_indices,
+    ) = create_system(
+        box_width,
+        box_height,
+        particle_width,
+        particle_height,
+        initial_angle,
+        angle_amplitude,
+        num_particles,
+    )
+
+    # find collisions with boundry
+    (
+        new_particle_coordinates,
+        valid_x_boundry_collision,
+        valid_y_boundry_collision,
+    ) = box_boundry.get_final_coordinates(
+        particle_coordinates, non_collided_particle_indices
+    )
+
+    # either x or y collision occured for all relevant indices
+    assert np.all(np.logical_or(valid_x_boundry_collision, valid_y_boundry_collision))
+
+    # no x AND y collisions occured
+    assert np.all(valid_x_boundry_collision != valid_y_boundry_collision)
+
+    # assert only y collisions occured
+    assert np.all(valid_y_boundry_collision)
+
+    x_coordinates = new_particle_coordinates[non_collided_particle_indices, 0]
+    y_coordinates = new_particle_coordinates[non_collided_particle_indices, 1]
+    assert np.all(np.abs(x_coordinates) <= (box_width / 2))
+    assert np.all(np.abs(y_coordinates) == (box_height / 2))
+
+    # check coordinates of reflector collided particles are left unchanged
+    assert np.all(
+        new_particle_coordinates[reflector_collided_particles_indices, :]
+        == particle_coordinates[reflector_collided_particles_indices, :]
+    )
+
+
+if __name__ == "__main__":
+    test_instantiation()
+    test_x_boundry_reached()
+    test_y_boundry_reached()
